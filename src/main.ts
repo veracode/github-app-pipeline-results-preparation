6fcb1607725f4ec2552190ca2f09255cb320b658
Vincent Deng <vincentdeng.it@gmail.com>
2024-01-16 18:02:09 +1100

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import { parseInputs } from './inputs';
+import { Octokit } from '@octokit/rest';
 
 /**
  * Runs the action.
@@ -8,4 +9,14 @@ export async function run(): Promise<void> {
   const inputs = parseInputs(core.getInput);
   console.log(inputs);
   console.log(inputs.token);
+  const octokit = new Octokit({
+    auth: inputs.token,
+  });
+
+  const { data } = await octokit.repos.listForOrg({
+    org: 'vincent-deng',
+    type: 'public',
+  });
+
+  console.log(data);
 }
