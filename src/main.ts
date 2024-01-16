dacf2ab862fe0bfdd09ca3f9b4f98daf4fd79a89
Vincent Deng <vincentdeng.it@gmail.com>
2024-01-16 18:54:14 +1100

    debug

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,24 @@
 import * as core from '@actions/core';
 import { parseInputs } from './inputs';
-import { Octokit } from '@octokit/rest';
+// import { Octokit } from '@octokit/rest';
+import * as fs from 'fs/promises';
+
+// Interface for the data structure in results.json
+
+interface Finding {
+  // Define the properties of a single finding
+  title: string;
+  issue_id: number;
+  severity: number;
+  issue_type_id: string;
+  issue_type: string;
+  cwe_id: string;
+}
+interface ResultsData {
+  scan_id: string;
+  modules: string[];
+  findings: Finding[]; // Adjust the type of findings array if needed
+}
 
 /**
  * Runs the action.
@@ -9,29 +27,34 @@ export async function run(): Promise<void> {
   const inputs = parseInputs(core.getInput);
   console.log(inputs);
   console.log(inputs.token);
-  const octokit = new Octokit({
-    auth: inputs.token,
-  });
 
-  core.debug('Setting up OctoKit...');
-  // const octokit = github.getOctokit(inputs.token);
+  try {
+    const data = await fs.readFile('results.json', 'utf-8');
+    const parsedData: ResultsData = JSON.parse(data);
+    const findingsArray = parsedData.findings;
 
-  const ownership = {
-    owner: 'veracode-github-app-new',
-    repo: 'veracode'
-  };
+    console.log(findingsArray); // Access and process the findings array
 
-  // get artifacts in a workflow run
-  const { data } = await octokit.actions.listWorkflowRunArtifacts({
-    owner: ownership.owner,
-    repo: 'veracode',
-    run_id: inputs.run_id,
-  });
-  console.log(data);
-
-
-  // const { data } = await octokit.repos.listForOrg({
-  //   org: 'vincent-deng',
-  //   type: 'public',
+  } catch (error) {
+    console.error('Error reading or parsing results.json:', error);
+  }
+  // const octokit = new Octokit({
+  //   auth: inputs.token,
   // });
+
+  // core.debug('Setting up OctoKit...');
+  // // const octokit = github.getOctokit(inputs.token);
+
+  // const ownership = {
+  //   owner: 'veracode-github-app-new',
+  //   repo: 'veracode'
+  // };
+
+  // // get artifacts in a workflow run
+  // const { data } = await octokit.actions.listWorkflowRunArtifacts({
+  //   owner: ownership.owner,
+  //   repo: 'veracode',
+  //   run_id: inputs.run_id,
+  // });
+  // console.log(data);
 }
