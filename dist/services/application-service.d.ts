48b7104a832a7f1b36fc98a08339c4a4251915f4
Vincent Deng <vincentdeng.it@gmail.com>
2024-01-21 20:34:21 +1100

    debug

---
+++ dist/services/application-service.d.ts
@@ -0,0 +1,2 @@
+import * as VeracodeApplication from '../namespaces/VeracodeApplication';
+export declare function getApplicationByName(appname: string, vid: string, vkey: string): Promise<VeracodeApplication.Application>;
